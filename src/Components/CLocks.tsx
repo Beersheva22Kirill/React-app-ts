e481c813c6efaccaa94e004e109b0a171b61d7a0
kirill2884 <kirill2884@gmail.com>
2023-06-18 12:50:25 +0300

    yuri solution

--- src/Components/CLocks.tsx
+++ src/Components/CLocks.tsx
@@ -1,18 +1,19 @@
-import {useState } from "react";
+import { useState, useEffect } from "react";
 import { Clock } from "./Clock"
 
-const Clocks: React.FC = () => {
- 
-    return <div style={
-        {display: 'flex', 
-        flexDirection: "row",
-        justifyContent:"space-around"
-        }}> 
-            <Clock countryOrCity={"undefined"}></Clock>
-            <Clock countryOrCity={"Mexico"}></Clock>
-            <Clock countryOrCity={"Antarctica"}></Clock>
-            <Clock countryOrCity={"Minsk"}/>
+const Clocks:React.FC = () => {
+    const [time, setTime] = useState<Date>(new Date())
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+       setTime(new Date());
+      
+   }, 1000 );
+   return () => clearInterval(intervalId)
+   }, [])
+    return <div style={{display: 'flex',
+     flexDirection: 'row', justifyContent: 'space-around'}}>
+       <Clock time={time} cityCountry="London" />
+       <Clock time={time} cityCountry="Toronto"/>
     </div>
-} 
-
+}
 export default Clocks
--- src/Components/Clock.tsx
+++ src/Components/Clock.tsx
@@ -1,45 +1,29 @@
-import { CSSProperties, useEffect, useMemo, useState } from "react"
-import timeZones from "../Services/time-zones"
-
+import { CSSProperties} from "react";
+import timeZones from '../Services/time-zones';
 type Props = {
-    countryOrCity: string,
-
+    time: Date,
+    cityCountry: string
+};
+const style: CSSProperties = {display: "flex",
+     flexDirection: "column", alignItems: 'center'};
+function getTimeZone(cityCountry: string): string|undefined {
+    const timeZoneObj =
+     timeZones.find(tz => JSON.stringify(tz).includes(cityCountry));
+     return timeZoneObj?.name;
 }
-export const Clock: React.FC<Props> = ({countryOrCity}) => {
-    const style: CSSProperties = {
-        display: "flex",
-        flexDirection:"column",
-        alignItems:"center",
-    }
-    let timezone:string = "Asia/Jerusalem";
-    let country:string = `country: Israel`;
+export const Clock: React.FC<Props> = ({time, cityCountry}) => {
+    const timeZone: string|undefined = getTimeZone(cityCountry);
+    const title: string = (timeZone && cityCountry) || 'Israel';
+    const timeStr: string = time.toLocaleTimeString(undefined,
+     {timeZone}) 
+     
+    
+    
 
-    const AllTimeZones = timeZones;
-       AllTimeZones.forEach(stringOfZone => {
-
-        if (stringOfZone.mainCities.includes(countryOrCity)){
-                timezone = stringOfZone.name;
-                country = `city: ${countryOrCity}`;
-        } else if(stringOfZone.countryName.includes(countryOrCity)){
-                timezone = stringOfZone.name;
-                country = `country: ${countryOrCity}`;
-        }             
-    })
-
-    const [time, setTime] = useState<string>(new Date().toLocaleTimeString(undefined,{timeZone:timezone}))
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-           setTime(new Date().toLocaleTimeString(undefined,{timeZone:timezone}));                      
-        }, 1000 );
-    return () => clearInterval(intervalId)
-   }, [countryOrCity])
-
- 
     return <div style={style}>
-                <header>
-                   {`Time in ${country}`}
-                </header>
-                <p>{time}</p>
-           </div>
+            <header>
+                Time in {title}
+            </header>
+            <p>{timeStr}</p>
+    </div>
 }
